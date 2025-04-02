3327171b502454e76b00981ba79e3cf2a7a2d925
LimboCR <mrpinulgleb@gmail.com>
2025-04-02 19:00:49 +0300

    Small Edits in TestScene

--- Assets/_Scripts/Mechanics/Spawn and Navigation/SpawnPresets.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "CharacterSystems/NPC Spawn Presets")]
-public class SpawnPresets : ScriptableObject
-{
-    public GameObject NPCPrefab;
-    public ScriptableNPCDefinition npcSO;
-}
